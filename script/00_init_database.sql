7234d7ad3f4821581e0172581c425e8d007adbec
quynhvpham <quynh.vpham97@gmail.com>
2026-08-03 12:56:14 -0500

    Update 00_init_database.sql

--- script/00_init_database.sql
+++ script/00_init_database.sql
@@ -6,12 +6,6 @@ Script Purpose:
     This script creates a new database named 'DataWarehouseAnalytics' after checking if it already exists. 
     If the database exists, it is dropped and recreated. Additionally, this script creates a schema called gold
 	
-WARNING:
-    Running this script will drop the entire 'DataWarehouseAnalytics' database if it exists. 
-    All data in the database will be permanently deleted. Proceed with caution 
-    and ensure you have proper backups before running this script.
-*/
-
 USE master;
 GO
 
